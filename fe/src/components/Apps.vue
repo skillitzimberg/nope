c48da50507a081eb25b431cf6db925e625077741
Powell Quiring <powellquiring@gmail.com>
2020-02-22 17:44:13 -0800

    demo

---
+++ fe/src/components/Apps.vue
@@ -0,0 +1,13 @@
+<template> </template>
+
+<script>
+export default {
+  data() {
+    return {
+      quote: 'I want single'
+    }
+  }
+}
+</script>
+
+<style></style>
--- fe/src/views/Home.vue
+++ fe/src/views/Home.vue
@@ -1,18 +1,34 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <HelloWorld :msg="msg" />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import HelloWorld from '@/components/HelloWorld.vue'
+import axios from 'axios'
 
 export default {
-  name: "Home",
+  name: 'Home',
   components: {
     HelloWorld
+  },
+  data() {
+    return {
+      msg: 'foo'
+    }
+  },
+  mounted() {
+    //'https://wu62p3f4x6.execute-api.us-west-2.amazonaws.com/stage/user?id=101'
+    axios
+      // .get('https://api.coindesk.com/v1/bpi/currentprice.json')
+      .get(
+        // 'https://api.coindesk.com/v1/bpi/currentprice.json'
+        'https://wu62p3f4x6.execute-api.us-west-2.amazonaws.com/stage/user?id=101',
+        { crossdomain: true }
+      )
+      .then(response => (this.msg = 'welcome:' + response.data.Item.name))
   }
-};
+}
 </script>
